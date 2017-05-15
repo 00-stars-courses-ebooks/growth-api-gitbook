a48aa834919b7575aaa702a35e1e7a4dff737e2d
Phodal Huang <h@phodal.com>
2017-05-15 15:50:08 +0800

    [T] create a simple parser to jsongst

---
+++ parse.js
@@ -0,0 +1,29 @@
+var parser = require('mdast');
+var fs = require('fs');
+var _ = require( 'lodash' )
+
+var raw = fs.readFileSync(__dirname + '/free-programming-books-zh_CN/README.md', 'utf8');
+// var raw = fs.readFileSync(__dirname + '/demo.md', 'utf8');
+var tokens = parser.parse(raw);
+
+var results = [];
+
+function listToTable(list){
+    var listItems = list.children;
+    _.each(listItems, item => {
+        var baseChildren = item.children[0].children[0];
+        if(baseChildren.children && baseChildren.children[0]) {
+            results.push({
+                href: baseChildren.href,
+                title: baseChildren.children[0].value
+            });
+        }
+    });
+    return results;
+}
+
+var result = _.map(_.filter(tokens.children, it => {
+    return it.type === 'list';
+}), listToTable);
+
+console.log(result);
