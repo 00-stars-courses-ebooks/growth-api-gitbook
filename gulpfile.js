4e99d22f947064b5dbb5be0835142c72f32f2ba8
Phodal Huang <h@phodal.com>
2017-05-15 11:40:15 +0800

    [T] regeneration api

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var md2json = require('gulp-markdown-table-to-json');
-
-gulp.task('default', function(){
-    gulp.src('README.md')
-        .pipe(md2json())
-        .pipe(gulp.dest('./api.json'));
-});
