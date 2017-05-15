839fdb9b6681e30614b112410b7298f530f625b7
Phodal Huang <h@phodal.com>
2017-05-15 16:17:45 +0800

    [T] fix size issue

--- parse.js
+++ parse.js
@@ -5,10 +5,10 @@ var _ = require( 'lodash' )
 var raw = fs.readFileSync(__dirname + '/free-programming-books-zh_CN/README.md', 'utf8');
 // var raw = fs.readFileSync(__dirname + '/demo.md', 'utf8');
 
-var results = [];
 var resultsInString = "";
 
 function listToTable(list){
+    var results = [];
     var listItems = list.children;
     _.each(listItems, item => {
         var baseChildren = item.children[0].children[0];
@@ -53,7 +53,7 @@ function listToTable(list){
 function getData(string) {
     var tokens = parser.parse(string)
     return _.map(_.filter(tokens.children, it => {
-        return it.type === 'list'
+        return it.type === 'list';
     }), listToTable)
 }
 
