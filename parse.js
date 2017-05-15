77b4b0c75a42186f4b315ea33d27b9ed42d1cefb
Phodal Huang <h@phodal.com>
2017-05-15 16:09:11 +0800

    [T] split different parse

--- parse.js
+++ parse.js
@@ -13,12 +13,11 @@ function listToTable(list){
     var listItems = list.children;
     _.each(listItems, item => {
         var baseChildren = item.children[0].children[0];
-        var gitRegex = /github\.com|gitbook\.io/;
         if(baseChildren.children && baseChildren.children[0]) {
             var title = baseChildren.children[0].value;
             var href = baseChildren.href;
 
-            if(gitRegex.test(baseChildren.href)) {
+            if(/github\.com/.test(baseChildren.href)) {
                 results.push({
                     href: href,
                     title: title,
@@ -26,7 +25,15 @@ function listToTable(list){
                 });
 
                 console.log(' ' + title + ' | ' +  href + ' | github ');
-            } else {
+            } else if(/gitbook\.io/.test(baseChildren.href)) {
+                results.push({
+                    href: href,
+                    title: title,
+                    type: 'gitbook'
+                });
+
+                console.log(' ' + title + ' | ' +  href + ' | gitbook ');
+            } else if(/^((http[s]?|ftp):\/)?\/?([^:\/\s]+)((\/\w+)*\/)([\w\-\.]+[^#?\s]+)(.*)?(#[\w\-]+)?$/.test(baseChildren.href)) {
                 results.push({
                     href: href,
                     title: title,
